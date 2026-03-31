9605a7f04650436d16aeb29c8acf0d1e098c2d3c
thisisatulkumar <theatulkumar11@gmail.com>
2026-03-31 15:00:22 +0530

    hotfix: make the 'add to cart' button functional and persist cart data using localStorage

--- src/store/cart.store.ts
+++ src/store/cart.store.ts
@@ -1,23 +1,32 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
 import { Cart, Item } from '../types/cart.types';
 
-const useCartStore = create<Cart>()((set) => ({
-	items: [],
+const useCartStore = create<Cart>()(
+	persist(
+		(set) => ({
+			items: [],
 
-	addItem: (item: Item) =>
-		set((state) => ({
-			items: [...state.items, item],
-		})),
+			addItem: (item: Item) =>
+				set((state) => ({
+					items: [...state.items, item],
+				})),
 
-	removeItem: (id: string) =>
-		set((state) => ({
-			items: state.items.filter((i) => i.product_id !== id),
-		})),
+			removeItem: (id: string) =>
+				set((state) => ({
+					items: state.items.filter((i) => i.product_id !== id),
+				})),
 
-	updateQuantity: (id: string, quantity: number) =>
-		set((state) => ({
-			items: state.items.map((i) => (i.product_id === id ? { ...i, quantity } : i)),
-		})),
-}));
+			updateQuantity: (id: string, quantity: number) =>
+				set((state) => ({
+					items: state.items.map((i) => (i.product_id === id ? { ...i, quantity } : i)),
+				})),
+		}),
+		{
+			name: 'cart-storage',
+		}
+	)
+);
 
 export default useCartStore;
