38ed1ec3a0567af82f1d3cd85f8b6d947a8a3fd2
thisisatulkumar <theatulkumar11@gmail.com>
2026-04-19 15:59:15 +0530

    feat: display the navbar only on home page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Logo from '@/components/navbar/Logo';
 import NavbarActions from '@/components/navbar/NavbarActions';
 import { Separator } from '@/components/ui/separator';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+	const pathname = usePathname();
+	if (pathname !== '/') return null;
+
 	return (
 		<header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
 			<div className="flex h-16 items-center justify-between px-6">
